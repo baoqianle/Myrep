06e50f05459c03623c37b12d00db505302fab3dd
bao <2468791452@qq.com>
2019-10-24 14:28:52 +0800

    修改了项目

--- HelloGIt/src/com/turing/Adev.java
+++
@@ -1,7 +0,0 @@
-package com.turing;
-
-public class Adev {
-    public static void main(String[] args) {
-        System.out.println("我是dev添加了一个类");
-    }
-}
---
+++ HelloGIt/src/com/turing/Amaster.java
@@ -0,0 +1,7 @@
+package com.turing;
+
+public class Amaster {
+    public static void main(String[] args) {
+        System.out.println("我是master添加了一个类");
+    }
+}
