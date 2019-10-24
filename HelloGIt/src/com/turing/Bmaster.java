775b594a349b788c19cfebee4de703c7ab3f2b5b
bao <2468791452@qq.com>
2019-10-24 15:45:12 +0800

    修改了项目

--- HelloGIt/src/com/turing/Bmaster.java
+++ HelloGIt/src/com/turing/Bmaster.java
@@ -1,7 +1,7 @@
 package com.turing;
 
-public class Amaster {
+public class Bmaster {
     public static void main(String[] args) {
-        System.out.println("desv创建了");
+        System.out.println("master创建了");
     }
 }
