3d5f356ce17e8e1df851bc686f10786d53942f95
bao <2468791452@qq.com>
2019-10-24 15:35:53 +0800

    修改了项目

--- HelloGIt/src/com/turing/Cheshi.java
+++ HelloGIt/src/com/turing/Cheshi.java
@@ -3,7 +3,8 @@ package com.turing;
 public class Cheshi {
     public void show(){
         System.out.println("rep2cheshishow执行了");
-        System.out.println("我是master中添加的一句话");
-        System.out.println("我是master中添加的二句话");
+        System.out.println("我是dev中添加的一句话");
+        System.out.println("我是dev中添加的二句话");
+        System.out.println("我是dev中添加的三句话");
      }
 }
