a378a88883fff6892581ad5cc84523966a678317
bao <2468791452@qq.com>
2019-10-24 12:12:56 +0800

    修改了

--- HelloGIt/src/com/turing/text.java
+++ HelloGIt/src/com/turing/text.java
@@ -3,5 +3,8 @@ package com.turing;
 public class text {
     public static void main(String[] args) {
         System.out.print("你好");
+        for (int i=0;i<10;i++){
+            System.out.print("你好"+i);
+        }
     }
 }
