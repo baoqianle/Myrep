9dad14487ddda0bdba7d1804a8ce3253742e424c
bao <2468791452@qq.com>
2019-10-24 14:27:35 +0800

    修改了项目

---
+++ HelloGIt/src/com/turing/Adev.java
@@ -0,0 +1,7 @@
+package com.turing;
+
+public class Adev {
+    public static void main(String[] args) {
+        System.out.println("我是dev添加了一个类");
+    }
+}
--- HelloGIt/src/com/turing/Cheshi.java
+++ HelloGIt/src/com/turing/Cheshi.java
@@ -3,5 +3,6 @@ package com.turing;
 public class Cheshi {
     public void show(){
         System.out.println("rep2cheshishow执行了");
-    }
+        System.out.println("我是dev中添加的一句话");
+     }
 }
